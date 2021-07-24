beb5047e24c0cf61db65d3ec603f56b619bc28a2
manudevelopia <manu@developia.info>
2021-07-24 09:16:04 +0200

    initialize repo

---
+++ src/main/java/info/developia/ratpack/poc/App.java
@@ -0,0 +1,17 @@
+package info.developia.ratpack.poc;
+
+import ratpack.server.RatpackServer;
+
+public class App {
+    public static String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        RatpackServer.start(server -> server.handlers(
+                chain -> chain.get(
+                        ctx -> ctx.render(getGreeting())
+                )
+        ));
+    }
+}
