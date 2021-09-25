b157d63887ec207125ef0799c40d8e8ca3910993
manudevelopia <manu@developia.info>
2021-09-25 12:05:29 +0200

    remove old sample code

--- src/main/java/info/developia/ratpack/poc/App.java
+++ src/main/java/info/developia/ratpack/poc/App.java
@@ -3,22 +3,12 @@ package info.developia.ratpack.poc;
 import info.developia.ratpack.poc.controller.TaskController;
 import info.developia.ratpack.poc.filter.CORSFilter;
 import info.developia.ratpack.poc.filter.RequestFilter;
-import info.developia.ratpack.poc.model.Task;
 import ratpack.server.RatpackServer;
-
-import java.util.List;
 
 public class App {
     public static String getGreeting() {
         return "Hello World!";
     }
-
-//    public static List<Task> getTasks() {
-//        return List.of(
-//                new Task("7c3706cb","title 1", "description 1"),
-//                new Task("7410dfaa","title 2", "description 2")
-//        );
-//    }
 
     public static void main(String[] args) throws Exception {
         final TaskController taskController = new TaskController();
