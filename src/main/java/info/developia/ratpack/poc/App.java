e929dcc8dae769725a45acd301b638e77cc778b4
manudevelopia <manu@developia.info>
2022-05-16 10:58:05 +0200

    test hodor library

--- src/main/java/info/developia/ratpack/poc/App.java
+++ src/main/java/info/developia/ratpack/poc/App.java
@@ -1,5 +1,7 @@
 package info.developia.ratpack.poc;
 
+import info.developia.hodor.annotation.Hodor;
+import info.developia.hodor.annotation.HoldTheDoor;
 import info.developia.ratpack.poc.controller.TaskController;
 import info.developia.ratpack.poc.filter.CORSFilter;
 import info.developia.ratpack.poc.filter.RequestFilter;
@@ -10,17 +12,28 @@ import ratpack.server.ServerConfig;
 import java.nio.file.Path;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    @Hodor
+    public static void main(String[] args) {
         final TaskController taskController = new TaskController();
 
         Path baseDir = BaseDir.find("public");
         ServerConfig config = ServerConfig.builder().baseDir(baseDir).development(true).build();
-        RatpackServer.start(server -> server.serverConfig(config).handlers(chain -> chain
-                .all(new CORSFilter())
-                .all(new RequestFilter())
-                .files(files -> files.dir("public").indexFiles("index.html"))
-                .path("tasks", ctx -> ctx.byMethod(action -> action.get(taskController::getAllTasks)))
-                .path("tasks/:id", ctx -> ctx.byMethod(action -> action.get(taskController::getTask)))
-        ));
+        try {
+            RatpackServer.start(server -> server.serverConfig(config).handlers(chain -> chain
+                    .all(new CORSFilter())
+                    .all(new RequestFilter())
+                    .files(files -> files.dir("public").indexFiles("index.html"))
+                    .path("tasks", ctx -> ctx.byMethod(action -> action.get(taskController::getAllTasks)))
+                    .path("tasks/:id", ctx -> ctx.byMethod(action -> action.get(taskController::getTask)))
+            ));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to start server. " + e.getMessage());
+        }
+    }
+
+    @HoldTheDoor
+    static void doSomething(Throwable t) {
+        System.out.println("Something went wrong: " + t.getMessage());
+        System.out.println("Sending a SOS!");
     }
 }
